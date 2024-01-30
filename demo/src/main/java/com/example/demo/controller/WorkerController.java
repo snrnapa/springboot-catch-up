b4bb792a5f2ab415ca04afe4904488e5ee541e46
snrnapa <shino.satoru@gmail.com>
2024-01-31 00:21:50 +0900

    userがh2databaseの予約語であるため、workerに戻した

--- demo/src/main/java/com/example/demo/controller/WorkerController.java
+++ demo/src/main/java/com/example/demo/controller/WorkerController.java
@@ -4,15 +4,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.entity.Users;
+import com.example.demo.entity.Worker;
 
 @RestController
-public class UserController {
+public class WorkerController {
 
     @GetMapping("/user")
     @ResponseBody
-    public Users getUser() {
-        Users user = new Users();
+    public Worker getUser() {
+        Worker user = new Worker();
         user.setId(12);
         user.setName("皆伝になりたい人です");
         user.setAge(27);
--- demo/src/main/java/com/example/demo/entity/Worker.java
+++ demo/src/main/java/com/example/demo/entity/Worker.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
-public class Users {
+public class Worker {
     @Id
     private Integer Id;
     private String Name;
