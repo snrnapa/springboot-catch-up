151c131e6e5723f7640f8c7f3083280a4acda863
snrnapa <shino.satoru@gmail.com>
2024-01-31 00:31:01 +0900

    なんか行けたわ

---
+++ demo/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+    worker(id, name, age)
+VALUES
+    (1, 'napa', 27);
+
+INSERT INTO
+    worker(id, name, age)
+VALUES
+    (2, 'ubo', 27);
+
+INSERT INTO
+    worker(id, name, age)
+VALUES
+    (3, 'mengo', 27);
---
+++ demo/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+CREATE TABLE worker (
+    id integer PRIMARY KEY,
+    name VARCHAR(12) NOT NULL,
+    age integer
+);
